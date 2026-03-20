71e75757f5151d4a672549e7f9d323c9734661b2
HAN JONGSEO <pantene74@kw.ac.kr>
2026-03-20 14:57:47 +0900

    Chore: BaseEntity 추가 (#1)

---
+++ src/main/java/com/kwcapstone/server/global/entity/BaseEntity.java
@@ -0,0 +1,24 @@
+package com.kwcapstone.server.global.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+public abstract class BaseEntity {
+    @CreatedDate
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at") // 없을 수도 있는 값
+    private LocalDateTime updatedAt;
+}
