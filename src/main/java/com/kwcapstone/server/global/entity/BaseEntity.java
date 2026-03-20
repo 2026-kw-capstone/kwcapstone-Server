03389f80249dc824d041051c6d5df453f3c09f87
HAN JONGSEO <pantene74@kw.ac.kr>
2026-03-20 15:02:04 +0900

    Chore: 생성 일자 데이터 무결성 강화 (#1)

--- src/main/java/com/kwcapstone/server/global/entity/BaseEntity.java
+++ src/main/java/com/kwcapstone/server/global/entity/BaseEntity.java
@@ -15,7 +15,7 @@ import java.time.LocalDateTime;
 @Getter
 public abstract class BaseEntity {
     @CreatedDate
-    @Column(name = "created_at", nullable = false)
+    @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
     @LastModifiedDate
